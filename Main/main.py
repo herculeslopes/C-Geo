5cb15edd5e4f401214fd7cb9dac5f01b45bed896
Alex Gomes <alexengcivil79@gmail.com>
2020-10-25 22:18:55 -0300

    Revert "update"
    This reverts commit c27e06c8b01b2f991f19e54259d5b31636abd99a.

--- Main/main.py
+++ Main/main.py
@@ -213,33 +213,16 @@ class MainProgram:
 
                         print('5')
 
-                    elif Ycg == y:
-                        if option == 'ACIMA':
-
-                            if (Ycg + fibra) > y:
-
-                                a = b - fibra
-
-                                S = a * x * ((a / 2) + fibra)
-                        
-                            print('6')
-                        
-                        elif option == 'ABAIXO':
-
-                            if ( Ycg - fibra) < y: 
-
-                                a = Ycg - fibra
-
-                                S = a * z * ((a / 2) + fibra) 
-
-                        print('7')
+                    else:
+                        S = "LECAO AINDA VAI ESCREVER A FÓRMULA"
+                        print('6')
 
                     sLabel['text'] = S
                     
                 else:
                     sLabel['fg'] = '#eb4034'
                     sLabel['text'] = 'NÃO É POSSÍVEL CALCULAR'
-                    print('8')
+                    print('7')
 
 
             # Converte Os Valores Das Caixas De Entrada
